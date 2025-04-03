b0728dabe57fc71ea55dcee2f8b8f147da81a88d
Via <alihantasyurek@gmail.com>
2025-04-03 20:04:11 +0300

    should do tests

--- script.js
+++ script.js
@@ -1,21 +1,108 @@
+let currNumber = "";
+const values = [];
+const validOperators = ["+", "-", "*", "/"];
+const operatorRegex = /[+\-*/]/;
+const display = document.querySelector("#display");
+const body = document.querySelector("#body");
+body.addEventListener("click", (e) => {
+  const val = e.target.value;
+  if (val) {
+    handleInput(val);
+  }
+});
+
+// if the index is 1 and user presses any other operator update the operator
+// const index = values.findIndex((el) => validOperators.includes(el));
+// if (index !== -1 && values.length === 2) {
+//   values[index] = val;
+//   return;
+// }
+
+function handleInput(val) {
+  const isOperator = validOperators.includes(val);
+  if (!isNaN(val)) { // if it's a digit
+    currNumber += val;
+    display.textContent = currNumber;
+  } else if (isOperator) {
+    pushNum();
+    if (!values.length) return;
+    if (callParse()) {
+      handleReturn(isOperator);
+    }
+    values.push(val); // operator
+  } else if (val === "=") {
+    pushNum();
+    callParse();
+  }
+}
+
+function pushNum() {
+  if (currNumber) {
+    values.push(currNumber);
+    currNumber = "";
+  }
+}
+
+//refactor this 2+2+3 = doesn't work
+function handleReturn(isOperator) {
+  if (isOperator) {
+    values.push(returnVal);
+  }
+}
+
+function callParse() {
+  if (values.length === 3) {
+    const str = values.join("");
+    parse(str);
+    return true;
+  }
+  return false;
+}
+
+function parse(str) {
+  let [x, y] = str.split(operatorRegex);
+  const operatorMatch = str.match(operatorRegex);
+  if (!operatorMatch) return cleanInput();
+  const operator = operatorMatch[0];
+  x = Number(x);
+  y = Number(y);
+  if (isNaN(x) || isNaN(y)) return cleanInput();
+  operate(x, operator, y);
+}
+
+// if (values.some((el) => validOperators.includes(el))) {
+//   return;
+// }
+
+// TODO HANDLE / 0
+
+let returnVal = 0; // TOFIX
 function add(x, y) {
   const res = x + y;
   cleanInput();
-  values.push(res);
-  console.log(res);
-  // return x + y;
+  returnVal = res;
+  display.textContent = res;
 }
 
 function subtract(x, y) {
-  return x - y;
+  const res = x - y;
+  cleanInput();
+  returnVal = res;
+  display.textContent = res;
 }
 
 function multiply(x, y) {
-  return x * y;
+  const res = x * y;
+  cleanInput();
+  returnVal = res;
+  display.textContent = res;
 }
 
 function divide(x, y) {
-  return x / y;
+  const res = x / y;
+  cleanInput();
+  returnVal = res;
+  display.textContent = res;
 }
 
 function operate(x, operator, y) {
@@ -39,54 +126,5 @@ function operate(x, operator, y) {
 
 function cleanInput() {
   values.length = 0; // []
+  display.textContent = "";
 }
-
-function parse(str) {
-  let [x, y] = str.split(operatorRegex);
-  const operatorMatch = str.match(operatorRegex);
-  if (!operatorMatch) return cleanInput();
-  const operator = operatorMatch[0];
-  x = Number(x);
-  y = Number(y);
-  if (isNaN(x) || isNaN(y)) return cleanInput();
-  operate(x, operator, y);
-}
-
-let currNumber = "";
-const values = [];
-const validOperators = ["+", "-", "*", "/"];
-const operatorRegex = /[+\-*/]/;
-
-const display = document.querySelector("#display");
-display.addEventListener("click", (e) => {
-  const val = e.target.value;
-  if (!isNaN(val)) { // if it's a digit
-    currNumber += val;
-  } else if (validOperators.includes(val)) {
-    if (currNumber) {
-      values.push(currNumber);
-    }
-    if (!values.length) return;
-    // if the index is 1 and user presses any other operator update the operator
-    const index = values.findIndex((el) => validOperators.includes(el));
-    if (index !== -1 && values.length === 2) {
-      values[index] = val;
-      return;
-    }
-
-    values.push(val); // operator
-    currNumber = "";
-  }
-  // eval with = or auto-eval and send it to parse
-});
-// old solution
-// if (values.length === 3) {
-//   const str = values.join("");
-//   parse(str);
-// }
-
-// if (values.some((el) => validOperators.includes(el))) {
-//   return;
-// }
-
-// TODO HANDLE / 0
