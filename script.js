0206f0d0d5def05db45ef75d0a075010b64cf6a5
Via <alihantasyurek@gmail.com>
2025-04-04 14:05:48 +0300

    almost done

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let currNumber = "";
+const maxValidNum = 11;
 const values = [];
 const validOperators = ["+", "-", "*", "/"];
 const operatorRegex = /[+\-*/]/;
 const display = document.querySelector("#display");
+display.textContent = 0;
 const body = document.querySelector("#body");
+
 body.addEventListener("click", (e) => {
   const val = e.target.value;
   if (val) {
@@ -11,28 +14,88 @@ body.addEventListener("click", (e) => {
   }
 });
 
-// if the index is 1 and user presses any other operator update the operator
-// const index = values.findIndex((el) => validOperators.includes(el));
-// if (index !== -1 && values.length === 2) {
-//   values[index] = val;
-//   return;
-// }
+function updateCurr() {
+  if (isPointEnabled === true && returnVal) {
+    if (returnVal.includes(".")) {
+      isPointEnabled = false;
+    }
+    currNumber = returnVal;
+  }
+}
+function checkSpecialInputs(val) {
+  if (val === "clear") {
+    fullClear();
+    return true;
+  } else if (val === "remove") {
+    if (currNumber) {
+      if (currNumber.slice(-1) === ".") isPointEnabled = true;
+      currNumber = currNumber.slice(0, -1);
+      display.textContent = currNumber || "0";
+      return true;
+    }
+  } else if (val === "point") {
+    if (isPointEnabled) {
+      updateCurr();
+      currNumber += currNumber ? "." : "0.";
+      display.textContent = currNumber;
+      isPointEnabled = false;
+    }
+    return true;
+  }
+  return false;
+}
 
+let isPointEnabled = true;
 function handleInput(val) {
   const isOperator = validOperators.includes(val);
+  if (checkSpecialInputs(val)) return;
   if (!isNaN(val)) { // if it's a digit
-    currNumber += val;
-    display.textContent = currNumber;
-  } else if (isOperator) {
-    pushNum();
-    if (!values.length) return;
-    if (callParse()) {
-      handleReturn(isOperator);
+    if (currNumber.length < maxValidNum) {
+      currNumber += val;
+      display.textContent = currNumber;
     }
+  } else {
+    processOperator(isOperator, val);
+    // if the index is 1 and user presses any other operator update the operator
+    const index = values.findIndex((el) => validOperators.includes(el));
+    if (index !== -1 && values.length === 2 && val !== "=") {
+      values[index] = val;
+    }
+  }
+}
+
+function processOperator(isOperator, val) {
+  pushNum();
+  pushVal(val);
+  callParse();
+  handleReturn(isOperator, val);
+}
+
+function handleReturn(isOperator, val) {
+  if (values.length > 0) {
+    returnVal = null;
+    return;
+  }
+  if (isOperator && returnVal) {
+    values.push(returnVal);
     values.push(val); // operator
   } else if (val === "=") {
-    pushNum();
-    callParse();
+    return;
+  }
+  returnVal = null;
+}
+
+function fullClear() {
+  cleanInput();
+  returnVal = null;
+  currNumber = "";
+  display.textContent = 0;
+  isPointEnabled = true;
+}
+
+function pushVal(val) {
+  if (values.length > 0 && values.length < 2) {
+    values.push(val);
   }
 }
 
@@ -40,13 +103,7 @@ function pushNum() {
   if (currNumber) {
     values.push(currNumber);
     currNumber = "";
-  }
-}
-
-//refactor this 2+2+3 = doesn't work
-function handleReturn(isOperator) {
-  if (isOperator) {
-    values.push(returnVal);
+    isPointEnabled = true;
   }
 }
 
@@ -54,9 +111,7 @@ function callParse() {
   if (values.length === 3) {
     const str = values.join("");
     parse(str);
-    return true;
   }
-  return false;
 }
 
 function parse(str) {
@@ -70,39 +125,53 @@ function parse(str) {
   operate(x, operator, y);
 }
 
-// if (values.some((el) => validOperators.includes(el))) {
-//   return;
-// }
-
-// TODO HANDLE / 0
-
-let returnVal = 0; // TOFIX
+let returnVal = null;
 function add(x, y) {
   const res = x + y;
+  resStr = res.toString();
   cleanInput();
-  returnVal = res;
-  display.textContent = res;
+  if (resStr.length > maxValidNum) {
+    const check = resStr.includes(".") || resStr.includes("-");
+    resStr = check ? resStr.slice(0, maxValidNum) : "9".repeat(maxValidNum);
+  }
+  returnVal = resStr;
+  display.textContent = resStr;
 }
 
 function subtract(x, y) {
   const res = x - y;
+  resStr = res.toString();
   cleanInput();
-  returnVal = res;
-  display.textContent = res;
+  if (resStr.length > maxValidNum) {
+    const check = resStr.includes(".") || resStr.includes("-");
+    resStr = check ? resStr.slice(0, maxValidNum) : "9".repeat(maxValidNum);
+  }
+  returnVal = resStr;
+  display.textContent = resStr;
 }
 
 function multiply(x, y) {
   const res = x * y;
+  resStr = res.toString();
   cleanInput();
-  returnVal = res;
-  display.textContent = res;
+  if (resStr.length > maxValidNum) {
+    const check = resStr.includes(".") || resStr.includes("-");
+    resStr = check ? resStr.slice(0, maxValidNum) : "9".repeat(maxValidNum);
+  }
+  returnVal = resStr;
+  display.textContent = resStr;
 }
 
 function divide(x, y) {
   const res = x / y;
+  resStr = res.toString();
   cleanInput();
-  returnVal = res;
-  display.textContent = res;
+  if (resStr.length > maxValidNum) {
+    const check = resStr.includes(".") || resStr.includes("-");
+    resStr = check ? resStr.slice(0, maxValidNum) : "9".repeat(maxValidNum);
+  }
+  returnVal = resStr;
+  display.textContent = resStr;
 }
 
 function operate(x, operator, y) {
